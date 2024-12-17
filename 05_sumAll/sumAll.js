e4895210cf808036c8fed1b4ebe8f72722417668
RedViperPT <40699748+RedViperPT@users.noreply.github.com>
2024-12-17 23:24:24 +0000

    Add  sumAll function to handle input validation and optimize sum calculation; enable all related tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function (min, max) {
+  if ((min < 0) || (max < 0)) return "ERROR";
+  if (!(Number.isInteger(min) && Number.isInteger(max))) return "ERROR";
 
+  // Ensure min is smaller than max
+  if (min > max) {
+    [min, max] = [max, min]; // Swap values
+  }
+
+  // Calculate the sum directly
+  let sum = (max * (max + 1)) / 2 - ((min - 1) * min) / 2;
+  return sum;
 };
 
 // Do not edit below this line
