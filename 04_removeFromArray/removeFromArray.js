08acd0af94774fcc111829d38a012bcc9eb79e20
RedViperPT <40699748+RedViperPT@users.noreply.github.com>
2024-12-17 11:41:43 +0000

    Implement removeFromArray function using for nested for loops to handle multiple values and update tests to validate functionality

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+//Solution using nested for loops, not efficient.
+const removeFromArray = function () {
+    let array = arguments[0];
+    for (let j = arguments.length - 1; j > 0; j--) {
+        for (let i = array.length - 1; i >= 0; i--) {
+            if (arguments[j] === array[i]) {
+                array.splice(i, 1);
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
