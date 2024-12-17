6d933ea96d28f006553c03f513c339808c93218c
RedViperPT <40699748+RedViperPT@users.noreply.github.com>
2024-12-17 01:23:15 +0000

    Add logic to reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const charArray = string.split("");
+    const arrayLenght = charArray.length;
+    let reversedArray = [];
+    for (let i = arrayLenght - 1; i >= 0; i--) {
+        reversedArray.push(charArray[i]);
+    }
+    const result = reversedArray.join("");
+    return result;
 };
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
