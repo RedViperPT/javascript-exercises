8ef0542b2734ca6570b29198962fb817a5e78538
RedViperPT <40699748+RedViperPT@users.noreply.github.com>
2024-12-17 00:33:38 +0000

    Solve the exercise using the repeat() method

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+//Solved using repeat()
 
+const repeatString = function (string, num) {
+    if (num < 0) {
+        return "ERROR"
+    }
+    else {
+        return string.repeat(num);
+    };
 };
 
 // Do not edit below this line
