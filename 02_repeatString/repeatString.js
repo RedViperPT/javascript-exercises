a779bec19591cd61e6f07d04f5b8096142096ea3
RedViperPT <40699748+RedViperPT@users.noreply.github.com>
2024-12-17 00:53:17 +0000

    Solve the exercise using a for loop instead of the repeat() method

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,12 +1,22 @@
 //Solved using repeat()
+// const repeatString = function (string, num) {
+//     if (num < 0) {
+//         return "ERROR"
+//     }
+//     else {
+//         return string.repeat(num);
+//     };
+// };
 
+
+//Solved using loop
 const repeatString = function (string, num) {
-    if (num < 0) {
-        return "ERROR"
+    if (num < 0) return "ERROR";
+    let result = ""
+    for (let i = 0; i < num; i++) {
+        result += string;
     }
-    else {
-        return string.repeat(num);
-    };
+    return result;
 };
 
 // Do not edit below this line
