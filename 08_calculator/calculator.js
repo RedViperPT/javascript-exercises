5290d76aae0be0f51dc0d530b0449cbd03dc6de9
RedViperPT <40699748+RedViperPT@users.noreply.github.com>
2025-01-16 23:58:20 +0000

    Add functions for add, subtract, sum, multiply, power and factorial

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,44 @@
-const add = function() {
-	
+const add = function (a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+	let sum = 0;
+	for (let i = 0; i < arr.length; i++) {
+		sum += arr[i];
+	}
+	return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+	let product = 1;
+	for (let i = 0; i < arr.length; i++) {
+		product *= arr[i];
+	}
+	return product;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+	if (n === 0 || n === 1) {
+		return 1;
+	}
+	return n * factorial(n - 1);
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial,
 };
