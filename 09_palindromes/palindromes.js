89c50e1f4e191d9ccdaafd7efc9ad1742e290175
RedViperPT <40699748+RedViperPT@users.noreply.github.com>
2025-01-17 11:42:40 +0000

    Enable all tests Write function to handle all expected cases

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (strOrig) {
+	const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+	let str = strOrig
+		.toLowerCase()
+		.split('')
+		.filter((char) => alphanumerical.includes(char))
+		.join('');
 
+	function reverseString(str) {
+		let newString = '';
+		for (let i = str.length - 1; i >= 0; i--) {
+			newString += str[i];
+		}
+		return newString;
+	}
+	return reverseString(str) === str;
 };
 
 // Do not edit below this line
