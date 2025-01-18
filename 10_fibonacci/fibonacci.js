01899ccd99c0c30693d987a316c1eefc907ee1fa
RedViperPT <40699748+RedViperPT@users.noreply.github.com>
2025-01-18 00:26:18 +0000

    Enable all test Add logic to generate fibonacci sequence, handle strings and negative values

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+	if (n < 0) {
+		return 'OOPS';
+	}
+	n = parseInt(n);
+	const phi = (1 + Math.sqrt(5)) / 2;
+	const psi = (1 - Math.sqrt(5)) / 2;
+	return Math.round((Math.pow(phi, n) - Math.pow(psi, n)) / Math.sqrt(5));
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
