1e32201e3bae80e90d1a9e1e56db11747cf53e79
RedViperPT <40699748+RedViperPT@users.noreply.github.com>
2025-01-18 20:21:33 +0000

    Add function to calculate oldest Add logic to handle undefined fields and possible empty arrays

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (list) {
+	if (!list.length) return null; // Handle empty array
+	return list.reduce((oldest, currentPerson) => {
+		const oldestAge = (oldest.yearOfDeath || new Date().getFullYear()) - oldest.yearOfBirth;
+		const currentAge = (currentPerson.yearOfDeath || new Date().getFullYear()) - currentPerson.yearOfBirth;
+		return currentAge > oldestAge ? currentPerson : oldest;
+	}, list[0]); // Start with the first person as the initial value
 };
 
 // Do not edit below this line
